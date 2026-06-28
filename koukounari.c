a1ef155a5a95cbc986275badbb90c7c24c3fd348
Konstantinos Kazazis <kazaziskonstantinos@proton.me>
2026-06-28 16:49:33 +0200

    reposition cursor, clear screen on die()

--- koukounari.c
+++ koukounari.c
@@ -13,6 +13,9 @@ struct termios orig_termios;
 
 /** terminal */
 void die(const char *s) {
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
     perror(s);
     exit(1);
 }
@@ -53,6 +56,7 @@ char editorReadKey() {
 /** output */
 void editorRefreshScreen() {
     write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
 /** input */
@@ -61,6 +65,8 @@ void editorProcessKeypress() {
 
     switch (c) {
         case CTRL_KEY('q'):
+            write(STDOUT_FILENO, "\x1b[2J", 4);
+            write(STDOUT_FILENO, "\x1b[H", 3);
             exit(0);
             break;
     }
